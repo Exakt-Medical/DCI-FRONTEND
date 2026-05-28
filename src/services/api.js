68df3d94d8fef930ac96a1a5190a024a9bc6c7d7
ExaktAdmin-74\MCCCC <mcamasis@exakt.com.ph>
2026-05-28 19:10:38 +0800

    fixed the auto-logout

--- src/services/api.js
+++ src/services/api.js
@@ -19,7 +19,7 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+    if (error.response && error.response.status === 401) {
       localStorage.removeItem("token");
       localStorage.removeItem("role");
       localStorage.removeItem("username");
@@ -27,7 +27,7 @@ api.interceptors.response.use(
       window.location.href = "/vvip-access/login";
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default api;
