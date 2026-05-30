297f2809ff25f05db1e503e6c0c8ff5fbfbd0f0a
EstakuyoExakt <jeustaquio@exakt.com.ph>
2026-05-30 17:55:52 +0800

    Done * Change Basic CTPL price to ₱60. * Remove the rest of other voucher cards.

--- src/services/paymentsService.js
+++ src/services/paymentsService.js
@@ -2,6 +2,7 @@ import api from "./api";
 
 export const paymentsService = {
   createTlpePayment: (request) => api.post("/payments/tlpe", request),
+  savePayment: (request) => api.post("/add", request),
 };
 
 export default paymentsService;
