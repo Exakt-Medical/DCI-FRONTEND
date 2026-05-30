eede8843fda83c6d2204eb54069e84e228e7b475
EstakuyoExakt <jeustaquio@exakt.com.ph>
2026-05-30 22:50:50 +0800

    removed add payment api

--- src/services/paymentsService.js
+++ src/services/paymentsService.js
@@ -2,7 +2,6 @@ import api from "./api";
 
 export const paymentsService = {
   createTlpePayment: (request) => api.post("/payments/tlpe", request),
-  savePayment: (request) => api.post("/add", request),
 };
 
 export default paymentsService;
